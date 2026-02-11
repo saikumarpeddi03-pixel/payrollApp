a1e3b7d1e0dccc1f50a9d1ac3f52f7280c0969e2
Saikumar199604 <saikumar.peddi@taodigitalsolutions.com>
2026-02-11 15:27:10 +0530

    bearer token update

--- src/main/java/com/payApp/payrollapp/config/OpenApiConfig.java
+++ src/main/java/com/payApp/payrollapp/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.payApp.payrollapp.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,23 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI payrollOpenApi(){
+
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("PayRoll Service Api")
-                        .description("API documentation for Payroll Processing System")
-                        .version("v1.0")
+                        .title("Payroll Management API")
+                        .version("1.0")
+                        .description("Payroll system APIs with JWT Authentication"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
                 );
     }
 }
